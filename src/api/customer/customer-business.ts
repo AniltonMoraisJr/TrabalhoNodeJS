17d7d1c5080e6809ce4969bfb656106dbe9c4939
Anilton Francisco de Morais Junior <anilton.morais@bettatecnologia.com.br>
2022-03-26 18:35:15 -0300

    feature: Criando novo método business para buscar dados no banco de dados

--- src/api/customer/customer-business.ts
+++ src/api/customer/customer-business.ts
@@ -1,16 +1,38 @@
 import { Customer } from "./customer-model";
-import { CustomerCreationRequestHandler } from "./customer-type";
+import {
+  CustomerCreationRequestHandler,
+  CustomerListAllRequestHandler,
+} from "./customer-type";
 
-const persistCustomer: CustomerCreationRequestHandler = async (req, res, next) => {
-    try {
-        const { customerToCreate } = res.locals;
-        res.locals.customerCreated = await Customer.create(customerToCreate);
-        next();
-    } catch(error) {
-        next(error);
-    }
-}
+const persistCustomer: CustomerCreationRequestHandler = async (
+  req,
+  res,
+  next
+) => {
+  try {
+    const { customerToCreate } = res.locals;
+    res.locals.customerCreated = await Customer.create(customerToCreate);
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
 
-export {
-    persistCustomer
-}
+const findAllCustomers: CustomerListAllRequestHandler = async (
+  req,
+  res,
+  next
+) => {
+  const { offset = 0, limit = 50 } = req.query;
+  try {
+    res.locals.listOfAllCustomers = await Customer.findAll({
+      offset: Number(offset),
+      limit: Number(limit),
+    });
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { persistCustomer, findAllCustomers };
