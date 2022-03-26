72c87b9666bbc8f912092be835b2fbfb1db69c36
Anilton Francisco de Morais Junior <anilton.morais@bettatecnologia.com.br>
2022-03-26 18:37:32 -0300

    feature: Adicionando novo método no controller para listar os customers

--- src/api/customer/customer-controller.ts
+++ src/api/customer/customer-controller.ts
@@ -1,20 +1,40 @@
-import { CREATED } from "http-status";
-import { persistCustomer } from "./customer-business";
+import { CREATED, OK } from "http-status";
+import { findAllCustomers, persistCustomer } from "./customer-business";
 import { createCustomerDeserializer } from "./customer-deserializer";
-import { createCustomerSerializer } from "./customer-serializer";
-import { CustomerCreationRequestHandler } from "./customer-type";
-import { createCustomerValidator } from "./customer-validator";
+import {
+  createCustomerSerializer,
+  listCustomersSerializer,
+} from "./customer-serializer";
+import {
+  CustomerCreationRequestHandler,
+  CustomerListAllRequestHandler,
+} from "./customer-type";
+import {
+  createCustomerValidator,
+  listCustomersValidator,
+} from "./customer-validator";
 
 const createCustomer = (): CustomerCreationRequestHandler[] => {
-    return [
-        createCustomerValidator(),
-        createCustomerDeserializer,
-        persistCustomer,
-        createCustomerSerializer,
-        (req, res) => { res.status(CREATED).json(res.locals.customerToRespond) }
-    ]
-}
-
-export {
-    createCustomer
+  return [
+    createCustomerValidator(),
+    createCustomerDeserializer,
+    persistCustomer,
+    createCustomerSerializer,
+    (req, res) => {
+      res.status(CREATED).json(res.locals.customerToRespond);
+    },
+  ];
 };
+
+const listCustomers = (): CustomerListAllRequestHandler[] => {
+  return [
+    listCustomersValidator(),
+    findAllCustomers,
+    listCustomersSerializer,
+    (req, res) => {
+      res.status(OK).json(res.locals.listOfAllCustomersToRespond);
+    },
+  ];
+};
+
+export { createCustomer, listCustomers };
