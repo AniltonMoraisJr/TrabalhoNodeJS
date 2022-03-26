996b1f2fa8048943dfc23d8188436ba158d567c7
Anilton Francisco de Morais Junior <anilton.morais@bettatecnologia.com.br>
2022-03-26 18:36:16 -0300

    feature: Criando novo método no validator para validar query params

--- src/api/customer/customer-validator.ts
+++ src/api/customer/customer-validator.ts
@@ -1,39 +1,33 @@
-import { celebrate, Joi, Segments } from 'celebrate';
+import { celebrate, Joi, Segments } from "celebrate";
 
 const createCustomerValidator = () => {
-    return celebrate({
-        [Segments.BODY]: Joi.object().keys({
-            name: Joi
-                .string()
-                .trim()
-                .min(2)
-                .max(100)
-                .required(),
-            document: Joi.object().keys({
-                cpf: Joi
-                    .string()
-                    .length(11)
-                    .regex(/^\d+$/),
-                cnpj: Joi
-                    .string()
-                    .length(14)
-                    .regex(/^\d+$/)
-            }).required().xor("cpf", "cnpj"),
-            contact: Joi.object().keys({
-                email: Joi
-                    .string()
-                    .email()
-                    .max(100)
-                    .required(),
-                phone: Joi
-                    .string()
-                    .trim()
-                    .max(15)
-            }).required()
+  return celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().trim().min(2).max(100).required(),
+      document: Joi.object()
+        .keys({
+          cpf: Joi.string().length(11).regex(/^\d+$/),
+          cnpj: Joi.string().length(14).regex(/^\d+$/),
         })
-    });
+        .required()
+        .xor("cpf", "cnpj"),
+      contact: Joi.object()
+        .keys({
+          email: Joi.string().email().max(100).required(),
+          phone: Joi.string().trim().max(15),
+        })
+        .required(),
+    }),
+  });
 };
 
-export {
-    createCustomerValidator
+const listCustomersValidator = () => {
+  return celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+      offset: Joi.number().optional().min(0),
+      limit: Joi.number().optional().min(0),
+    }),
+  });
 };
+
+export { createCustomerValidator, listCustomersValidator };
