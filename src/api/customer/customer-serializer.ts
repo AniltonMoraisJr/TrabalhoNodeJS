a1d220eff26dad48aa2d7c7e9b8eff75433e3b5c
Anilton Francisco de Morais Junior <anilton.morais@bettatecnologia.com.br>
2022-03-26 18:38:24 -0300

    feature: Criando novo método para deserializar o resultado vindo do banco e preparar para a resposta da rota

--- src/api/customer/customer-serializer.ts
+++ src/api/customer/customer-serializer.ts
@@ -1,24 +1,56 @@
-import { CustomerCreationRequestHandler } from "./customer-type";
+import {
+  CustomerCreationRequestHandler,
+  CustomerListAllRequestHandler,
+} from "./customer-type";
 
-const createCustomerSerializer: CustomerCreationRequestHandler = (req, res, next) => {
-    const { customerCreated } = res.locals;
-    res.locals.customerToRespond = {
-        uuid: customerCreated.uuid,
-        name: customerCreated.name,
-        contact: {
-            email: customerCreated.email,
-            phone: customerCreated.phone
-        },
-        document: {
-            cpf: customerCreated.cpf,
-            cnpj: customerCreated.cnpj
-        },
-        createdAt: customerCreated.createdAt.toISOString(),
-        updatedAt: customerCreated.updatedAt.toISOString()
-    }
-    next();
-}
+const createCustomerSerializer: CustomerCreationRequestHandler = (
+  req,
+  res,
+  next
+) => {
+  const { customerCreated } = res.locals;
+  res.locals.customerToRespond = {
+    uuid: customerCreated.uuid,
+    name: customerCreated.name,
+    contact: {
+      email: customerCreated.email,
+      phone: customerCreated.phone,
+    },
+    document: {
+      cpf: customerCreated.cpf,
+      cnpj: customerCreated.cnpj,
+    },
+    createdAt: customerCreated.createdAt.toISOString(),
+    updatedAt: customerCreated.updatedAt.toISOString(),
+  };
+  next();
+};
 
-export {
-    createCustomerSerializer
-}
+const listCustomersSerializer: CustomerListAllRequestHandler = async (
+  req,
+  res,
+  next
+) => {
+  const { listOfAllCustomers } = res.locals;
+  console.log(listOfAllCustomers);
+  res.locals.listOfAllCustomersToRespond = listOfAllCustomers.map(
+    (customer) => ({
+      uuid: customer.uuid,
+      name: customer.name,
+      contact: {
+        email: customer.email,
+        phone: customer.phone,
+      },
+      document: {
+        cpf: customer.cpf,
+        cnpj: customer.cnpj,
+      },
+      createdAt: customer.createdAt.toISOString(),
+      updatedAt: customer.updatedAt.toISOString(),
+    })
+  );
+  console.log(res.locals.listOfAllCustomersToRespond);
+  next();
+};
+
+export { createCustomerSerializer, listCustomersSerializer };
