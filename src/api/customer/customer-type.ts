58aa4a4dd1828f5348c5596b40255b929846e38b
Anilton Francisco de Morais Junior <anilton.morais@bettatecnologia.com.br>
2022-03-26 18:33:59 -0300

    feature: Criando novos tipos para o retornar lista de customers

--- src/api/customer/customer-type.ts
+++ src/api/customer/customer-type.ts
@@ -1,48 +1,74 @@
 import { RequestHandler } from "express";
-import { Model } from "sequelize-typescript"
-import { Optional } from "sequelize/types"
+import { Model } from "sequelize-typescript";
+import { Optional } from "sequelize/types";
 
 type CustomerAttributes = {
-    uuid: string;
-    name: string;
-    cpf?: string;
-    cnpj?: string;
-    email: string;
-    phone?: string;
-    createdAt: Date;
-    updatedAt: Date;
-}
+  uuid: string;
+  name: string;
+  cpf?: string;
+  cnpj?: string;
+  email: string;
+  phone?: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
 
-type CustomerCreationAttributes = Optional<CustomerAttributes, 'uuid' | 'createdAt' | 'updatedAt'>;
+type ListCustomerAttributes = CustomerAttributes[];
+
+type CustomerCreationAttributes = Optional<
+  CustomerAttributes,
+  "uuid" | "createdAt" | "updatedAt"
+>;
 
 type CustomerCreationRequest = {
-    name: string;
-    document: {
-        cpf?: string;
-        cnpj?: string;
-    },
-    contact: {
-        email: string;
-        phone?: string;
-    }
-}
+  name: string;
+  document: {
+    cpf?: string;
+    cnpj?: string;
+  };
+  contact: {
+    email: string;
+    phone?: string;
+  };
+};
 
 type CustomerResponse = CustomerCreationRequest & {
-    uuid: string;
-    createdAt: string;
-    updatedAt: string;
-}
+  uuid: string;
+  createdAt: string;
+  updatedAt: string;
+};
 
-export abstract class CustomerModel extends Model<CustomerAttributes, CustomerCreationAttributes>{};
+type CustomerListReponse = CustomerResponse[];
+
+type CustomerListQueryParams = {
+  offset?: string;
+  limit?: string;
+};
+
+export abstract class CustomerModel extends Model<
+  CustomerAttributes,
+  CustomerCreationAttributes
+> {}
 
 export type CustomerCreationRequestHandler = RequestHandler<
-    {}, // path params
-    CustomerResponse, // response
-    CustomerCreationRequest, // request
-    {}, // query params
-    {
-        customerToCreate: CustomerCreationAttributes
-        customerCreated: CustomerAttributes,
-        customerToRespond: CustomerResponse
-    }
+  {}, // path params
+  CustomerResponse, // response
+  CustomerCreationRequest, // request
+  {}, // query params
+  {
+    customerToCreate: CustomerCreationAttributes;
+    customerCreated: CustomerAttributes;
+    customerToRespond: CustomerResponse;
+  }
+>;
+
+export type CustomerListAllRequestHandler = RequestHandler<
+  {}, // path params
+  CustomerListReponse, // response
+  {}, // request
+  CustomerListQueryParams, // query params
+  {
+    listOfAllCustomers: ListCustomerAttributes;
+    listOfAllCustomersToRespond: CustomerListReponse;
+  }
 >;
